5b151f9c112d02c503be728ac88c98a3058f448d
panda2k <thelowkeypanda@gmail.com>
2021-05-10 18:40:53 -0700

    change interaction response flow now ack a response while api calls are being made this is done to prevent commands from timing out because of lambda cold start

--- bot/src/routes/interactions.ts
+++ bot/src/routes/interactions.ts
@@ -71,17 +71,19 @@ interactionRouter.post('', bodyParser.raw({type: 'application/json'}), async(req
 
     if (data.type == 1) { // needed for verification
         return res.json({type: 1})
-    } else if (data.data.name == 'help') {
-        await discord.respondToInteraction(data.id, data.token, {
+    } 
+    
+    await discord.respondToInteraction(data.id, data.token, { // ack a response to account for lambda cold start
+        content: ''
+    }, 5)
+    
+    if (data.data.name == 'help') {
+        await discord.updateInteractionMessage(data.application_id, data.token, {
             content: "View a list of commands by typing / and clicking the FBI badge on the left side of the popup"
         })
 
         return res.status(200)
     } else if (data.data.name == 'gamestats') {
-        await discord.respondToInteraction(data.id, data.token, { // ack a response as games are processed
-            content: ''
-        }, 5)
-
         await gameralert.getGames(data.member.user.id, data.data.options[0].value, data.data.options[1].value)
             .then(async (result) => {
                 if (result.games.length == 0) {
@@ -113,7 +115,7 @@ interactionRouter.post('', bodyParser.raw({type: 'application/json'}), async(req
             .catch(async error => {
                 if (error.response) {
                     if (error.response.statusCode == 404) {
-                        await discord.respondToInteraction(data.id, data.token, {
+                        await discord.updateInteractionMessage(data.application_id, data.token, {
                             content: 'They have no recorded games because they have not registered with Gamer Alert yet'
                         })
                         return
@@ -124,19 +126,19 @@ interactionRouter.post('', bodyParser.raw({type: 'application/json'}), async(req
                     console.log(error)
                 }
 
-                await discord.respondToInteraction(data.id, data.token, {
+                await discord.updateInteractionMessage(data.application_id, data.token, {
                     content: 'Error when getting game data. Try again in a few seconds'
                 })
             })
     } else if (data.data.name == 'timezone') {
         await gameralert.updateTimezone(data.member.user.id, data.data.options[0].value)
             .then(async() => {
-                await discord.respondToInteraction(data.id, data.token, {
+                await discord.updateInteractionMessage(data.application_id, data.token, {
                     content: `Updated server timezone to ${data.data.options[0].value}`
                 })
             })
             .catch(async() => {
-                await discord.respondToInteraction(data.id, data.token, {
+                await discord.updateInteractionMessage(data.application_id, data.token, {
                     content: `Invalid timezone. Check here for a list of timezone: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones`
                 })
             })
@@ -176,44 +178,44 @@ interactionRouter.post('', bodyParser.raw({type: 'application/json'}), async(req
                             .then(async() => {
                                 await gameralert.addUserToServer(discordId, data.guild_id)
                                     .then(async() => {
-                                        await discord.respondToInteraction(data.id, data.token, {
+                                        await discord.updateInteractionMessage(data.application_id, data.token, {
                                             content: `Successfully registered <@${discordId}>`
                                         })
                                     })
                                     .catch(async error => {
                                         if (error.response) {
                                             if (error.response.body.error.includes('already registered')) {
-                                                return await discord.respondToInteraction(data.id, data.token, {
+                                                return await discord.updateInteractionMessage(data.application_id, data.token, {
                                                     content: `<@${discordId}> is already registered in this server. However, their league name was still set to ${data.data.options[0].value}`
                                                 })
                                             }
                                         }
                                         console.log(error)
-                                        await discord.respondToInteraction(data.id, data.token, {
+                                        await discord.updateInteractionMessage(data.application_id, data.token, {
                                             content: 'Error when registering user'
                                         })
                                     })
                             })
                             .catch(async error => {
                                 if (error.response) {
-                                    await discord.respondToInteraction(data.id, data.token, {
+                                    await discord.updateInteractionMessage(data.application_id, data.token, {
                                         content: `Error: ${error.response.body.error}`
                                     })
                                 } else {
                                     console.log(error)
-                                    await discord.respondToInteraction(data.id, data.token, {
+                                    await discord.updateInteractionMessage(data.application_id, data.token, {
                                         content: 'Error when setting league username'
                                     })
                                 }
                             })
                         } else {
-                            await discord.respondToInteraction(data.id, data.token, {
+                            await discord.updateInteractionMessage(data.application_id, data.token, {
                                 content: 'Error when creating user'
                             })
                         }
                     })
                 } else {
-                    await discord.respondToInteraction(data.id, data.token, {
+                    await discord.updateInteractionMessage(data.application_id, data.token, {
                         content: 'Error when creating server'
                     })
                 }
@@ -221,13 +223,13 @@ interactionRouter.post('', bodyParser.raw({type: 'application/json'}), async(req
     } else if (data.data.name == "timelimit") {
         await gameralert.setTimeLimit(data.member.user.id, data.data.options[0].value)
             .then(async() => {
-                await discord.respondToInteraction(data.id, data.token, {
+                await discord.updateInteractionMessage(data.application_id, data.token, {
                     content: 'Successfully changed time limit'
                 })
             })
             .catch(async error => {
                 console.log(error)
-                await discord.respondToInteraction(data.id, data.token, {
+                await discord.updateInteractionMessage(data.application_id, data.token, {
                     content: 'Error when updating time limit'
                 })
             })
@@ -242,25 +244,30 @@ interactionRouter.post('', bodyParser.raw({type: 'application/json'}), async(req
 
                 const timeString = prettyTime(playTime)
 
-                await discord.respondToInteraction(data.id, data.token, {
+                await discord.updateInteractionMessage(data.application_id, data.token, {
                     content: `They've played ${timeString} of League in the specified time period`
                 })
+                    .catch(error => {
+                        console.log('Error responding to interaction')
+                        console.log(error)
+                        console.log(JSON.stringify(error.response.body))
+                    })
             })
             .catch(async error => {
                 if (error.response) {
                     if (error.response.statusCode == 404) {
-                        await discord.respondToInteraction(data.id, data.token, {
+                        await discord.updateInteractionMessage(data.application_id, data.token, {
                             content: 'No play time logged because they have not yet registered for Gamer Alert'
                         })
                     } else {
                         console.log(error.response.body.erro)
-                        await discord.respondToInteraction(data.id, data.token, {
+                        await discord.updateInteractionMessage(data.application_id, data.token, {
                             content: 'Error when fetching playtime'
                         })
                     }
                 } else {
                     console.log(error)
-                    await discord.respondToInteraction(data.id, data.token, {
+                    await discord.updateInteractionMessage(data.application_id, data.token, {
                         content: 'Error when fetching playtime'
                     })
                 }
